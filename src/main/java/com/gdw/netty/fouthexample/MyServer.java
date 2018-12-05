7691526643033a416cac6a206c65be8e5a6c3467
gdw <david.gao@trustlife.com>
2018-12-05 15:03:55 +0800

    完成第四个实例

---
+++ src/main/java/com/gdw/netty/fouthexample/MyServer.java
@@ -0,0 +1,54 @@
+package com.gdw.netty.fouthexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
+
+public class MyServer {
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap =
+                    new ServerBootstrap()
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new MyServerInitializer());
+            ChannelFuture channelFuture = serverBootstrap.bind(8089).sync();
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
+
+class MyServerInitializer extends ChannelInitializer<SocketChannel>{
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new IdleStateHandler(5,7, 10));
+        pipeline.addLast(new MySeverHandler());
+    }
+}
+
+class MySeverHandler extends ChannelInboundHandlerAdapter{
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if(evt instanceof IdleStateEvent){
+            IdleStateEvent event = (IdleStateEvent)evt;
+            System.out.println(ctx.channel().remoteAddress() + "超时事件： " + event.state());
+            ctx.channel().close();
+        }
+    }
+}
